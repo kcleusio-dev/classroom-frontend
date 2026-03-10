83426a2c4bbd00bd4dac9b42719749dc45cdbdeb
kcleusio-dev <kcleusio.dev@gmail.com>
2026-03-10 14:36:52 +0100

    Ajustar departamentos para serem iguais nos dois arquivos

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const DEPARTMENTS = [
-    'CS',
-    'Math',
-    'English'
+    'Computer Science',
+    'Mathematics',
+    'Physics'
 ];
 
 export const DEPARTMENTS_OPTIONS = DEPARTMENTS.map((dept) => ({
